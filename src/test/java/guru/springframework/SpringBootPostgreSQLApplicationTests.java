982e38de339fd4d59755d600bf7746b36bc2f122
mfarid <mohd.farid@devfactory.com>
2017-02-15 18:28:45 +0530

    Add PostgreSQL specific configuration

--- src/test/java/guru/springframework/SpringBootPostgreSQLApplicationTests.java
+++ src/test/java/guru/springframework/SpringBootPostgreSQLApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class SpringBootMySqlApplicationTests {
+public class SpringBootPostgreSQLApplicationTests {
 
 	@Test
 	public void contextLoads() {
